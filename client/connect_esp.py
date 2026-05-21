37cd5c4c7e665397d5237928feb3f178640f8e5d
Matthew Zahra <matthewzahra04@gmail.com>
2026-05-21 02:28:42 +0100

    added python test file to interact with the WS implementation

---
+++ client/connect_esp.py
@@ -0,0 +1,26 @@
+'''
+open a web socket connection to the ESP32
+'''
+
+import asyncio
+import websockets
+
+# TODO - fill this in
+esp_address = 'ws://192.168.4.1/ws'
+
+async def sender(ws):
+    await ws.send('this is a test message...')
+
+async def receiver(ws):
+    async for msg in ws:
+        print(f"received: {msg}")
+
+async def connect():
+    async with websockets.connect(esp_address) as websocket:
+        print("###################### CONNECTED ######################")
+        send_task = asyncio.create_task(sender(websocket))
+        recv_task = asyncio.create_task(receiver(websocket))
+        await asyncio.gather(send_task, recv_task)  # wait for both to finish
+
+if __name__ == "__main__":
+    asyncio.run(connect())
